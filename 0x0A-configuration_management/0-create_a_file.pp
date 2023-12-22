cd8b258720ce112cf18708812c4b77500c9d830c
Kyalo460 <Kyalo460@gmail.com>
2023-12-22 19:30:28 +0300

    All files

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# Creating a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
+
