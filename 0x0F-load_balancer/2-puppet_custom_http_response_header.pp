4399f96beddd1a15d39df8c256c002ca99d65ca5
Kyalo460 <Kyalo460@gmail.com>
2024-01-08 11:20:01 +0300

    used puppet to configure nginx

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+# This script uses puppet to
+# automate the task of creating a custom HTTP header response
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run2':
+  command => '/usr/sbin/service nginx start',
+}
+
