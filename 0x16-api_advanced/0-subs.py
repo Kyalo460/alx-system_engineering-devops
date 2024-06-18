d9a5aed6b21d1189ed2d20a5a4307936af9dc493
Kyalo460 <Kyalo460@gmail.com>
2024-06-18 17:45:44 +0300

    longer comment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Function that queries' reddits api for subs."""
+"""A function that queries the Reddit API and returns the
+number of subscribers (not active users, total subscribers)
+for a given subreddit.
+"""
 import requests
 
 
