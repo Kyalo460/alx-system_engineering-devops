c417f521240dd4585eecc2189d44787a52bf9201
Kyalo460 <Kyalo460@gmail.com>
2024-06-19 14:36:14 +0300

    Shortened code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subs."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "0-subs.py/1.0"
-        }
-
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-agent": "Linux:0x16-advanced_api"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code != 200:
         return 0
-
     return response.json().get('data').get('subscribers')
